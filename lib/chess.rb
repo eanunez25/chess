f38b1704a3456ddabd34bdc6786d7d9654412175
Elvin Nunez <eanunez25@gmail.com>
2020-05-24 11:50:57 -0500

    create create_pieces method for white pieces

--- lib/chess.rb
+++ lib/chess.rb
@@ -23,20 +23,77 @@ class Board
   end
 
   def create_pieces
+    @white_king = King.new("king", "white", "\u2654", [7,3])
+    @white_queen = Queen.new("queen", "white", "\u2655", [7,4])
+    @white_rook1 = Rook.new("rook", "white", "\u2656", [7,0])
+    @white_rook2 = Rook.new("rook", "white", "\u2656", [7,7])
+    @white_bishop1 = Bishop.new("bishop", "white", "\u2657", [7,2])
+    @white_bishop2 = Bishop.new("bishop", "white", "\u2657", [7,5])
+    @white_knight1 = Knight.new("knight", "white", "\u2658", [7,1])
+    @white_knight2 = Knight.new("knight", "white", "\u2658", [7,6])
+    @white_pawn1 = Pawn.new("pawn", "white", "\u2659", [6,0])
+    @white_pawn2 = Pawn.new("pawn", "white", "\u2659", [6,1])
+    @white_pawn3 = Pawn.new("pawn", "white", "\u2659", [6,2])
+    @white_pawn4 = Pawn.new("pawn", "white", "\u2659", [6,3])
+    @white_pawn5 = Pawn.new("pawn", "white", "\u2659", [6,4])
+    @white_pawn6 = Pawn.new("pawn", "white", "\u2659", [6,5])
+    @white_pawn7 = Pawn.new("pawn", "white", "\u2659", [6,6])
+    @white_pawn8 = Pawn.new("pawn", "white", "\u2659", [6,7])
 
   end
 
 end
 
 class Piece 
+  attr_accessor :type, :player, :image, :location
 
-  def initialize(type, player, image)
+  def initialize(type, player, image, location)
     @type = type
     @player = player
     @image = image
+    @location = location
   end
 
 end
 
+class King < Piece
+  def initialize(type, player, image, location)
+    super
+  end
+end
+
+class Queen < Piece
+  def initialize(type, player, image, location)
+    super
+  end
+end
+
+class Rook < Piece
+  def initialize(type, player, image, location)
+    super
+  end
+end
+
+class Bishop < Piece
+  def initialize(type, player, image, location)
+    super
+  end
+end
+
+class Knight < Piece
+  def initialize(type, player, image, location)
+    super
+  end
+end
+
+class Pawn < Piece
+  def initialize(type, player, image, location)
+    super
+  end
+end
+
+
 board = Board.new
+board.create_pieces
 board.display_board
+
