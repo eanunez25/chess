673526f1877661d4821bbe1179827f1ea64212cf
Elvin Nunez <eanunez25@gmail.com>
2020-05-24 12:03:08 -0500

    initialize Player class

--- lib/chess.rb
+++ lib/chess.rb
@@ -92,6 +92,15 @@ class Pawn < Piece
   end
 end
 
+class Player 
+  attr_accessor :name, :color
+
+  def initialize(name, color)
+    @name = name
+    @coloro = color
+  end
+end
+
 
 board = Board.new
 board.create_pieces
