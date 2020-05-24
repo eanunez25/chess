9e111828c146fb7afd5954fe66e5b6a58b4c6011
Elvin Nunez <eanunez25@gmail.com>
2020-05-24 10:55:34 -0500

    create Piece class

--- lib/chess.rb
+++ lib/chess.rb
@@ -7,14 +7,30 @@ class Board
 
   def create_board
     board = Array.new(8) { Array.new(8) }
+    
     8.times do |idx|
       8.times do |value|
         board[idx][value] = "-"
       end
     end
+
     8.times do |num|
       puts board[num].join
     end
+  end
+
+  def create_pieces
+
+  end
+
+end
+
+class Piece 
+
+  def initialize(type, player, image)
+    @type = type
+    @player = player
+    @image = image
   end
 
 end
